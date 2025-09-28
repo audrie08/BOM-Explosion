13231495c0a8fba4fe1deceeaf53ddd08cb7b7ee
audrie08 <olonaaudreyy@gmail.com>
2025-09-29 00:53:25 +0800

    Update bomexplosion.py

--- bomexplosion.py
+++ bomexplosion.py
@@ -610,8 +610,6 @@ if station == "Cold Kitchen" and selected_recipe and selected_recipe != "No reci
     
     with recipe_col2:
         st.markdown("**Recipe (# of Batches):**")
-        with recipe_col2:
-        st.markdown("**Recipe (# of Batches):**")
         num_batches = st.number_input("", min_value=1, value=int(float(bom_data['recipe_batches'])) if bom_data['recipe_batches'] else 1, step=1, key="batches_input")
     
     st.markdown('</div></div>', unsafe_allow_html=True)
